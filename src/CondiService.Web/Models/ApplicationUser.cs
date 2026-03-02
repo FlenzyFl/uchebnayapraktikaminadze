29203377ff53b0efa8e2d5042f51ccd55860b12c
FlenzyFl <87827657+FlenzyFl@users.noreply.github.com>
2026-03-02 17:07:05 +0500

    Models

---
+++ src/CondiService.Web/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace CondiService.Web.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    public int? ExternalUserId { get; set; }
+
+    public string FullName { get; set; } = string.Empty;
+}
---
+++ src/CondiService.Web/Models/CommentType.cs
@@ -0,0 +1,8 @@
+namespace CondiService.Web.Models;
+
+public enum CommentType
+{
+    Note = 0,
+    HelpRequest = 1,
+    QualityManager = 2
+}
